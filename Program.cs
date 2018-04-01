4a9239e35f82f5b5dee8c47251ff2e7418ce419b
dia76 <37978555+dia76@users.noreply.github.com>
2018-04-01 16:13:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        int timeForABreak = int.Parse(Console.ReadLine());
+        double cardPrice = double.Parse(Console.ReadLine());
+        double adventurePrice = double.Parse(Console.ReadLine());
+        double coffeePrice = double.Parse(Console.ReadLine());
+
+        double moneyForCard = cardPrice * 3;
+        double moneyForadventure = adventurePrice * 2;
+        double totalMoney = moneyForCard + moneyForadventure + coffeePrice;
+        double totalTime = timeForABreak - 5 - 6 - 4;
+
+        Console.WriteLine("{0:f2}", totalMoney);
+        Console.WriteLine(totalTime);
+    }
+}
+
